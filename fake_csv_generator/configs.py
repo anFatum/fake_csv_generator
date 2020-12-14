398932e47f68b70167090181a3d1dfb0ee73a092
Anastasia <ans.dudarenko@gmail.com>
2020-12-14 02:58:19 +0200

    Allowed hosts added in config

--- fake_csv_generator/configs.py
+++ fake_csv_generator/configs.py
@@ -4,6 +4,7 @@ import os
 dotenv.load_dotenv(dotenv.find_dotenv())
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", False)
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(',')
 
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
--- fake_csv_generator/settings.py
+++ fake_csv_generator/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = configs.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = configs.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = configs.ALLOWED_HOSTS
 
 # Application definition
 
